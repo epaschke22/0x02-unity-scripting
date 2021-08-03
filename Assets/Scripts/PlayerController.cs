86cf77bf12181e59121c362b741c99150b853605
epaschke22 <ericksonpaschke@gmail.com>
2021-08-03 12:44:53 -0400

    Added Player movement with new Input system

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    CharacterController character;
+    Vector3 moveVector;
+    public float speed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        character.Move(moveVector * speed * Time.fixedDeltaTime);
+    }
+
+    public void OnMovementChanged(InputAction.CallbackContext context)
+	{
+        Vector2 direction = context.ReadValue<Vector2>();
+        moveVector = new Vector3(direction.x, 0, direction.y);
+	}
+}
